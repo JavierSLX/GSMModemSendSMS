991adbaea43cf018d0d137bfdd3c8c85749c8eaa
JavierSLX <jserranolule@gmail.com>
2018-07-25 14:54:05 -0500

    Inicial

---
+++ GSMMessage/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GSMMessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phone Number";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(116, 22);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(177, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(352, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(393, 25);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(116, 49);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(377, 80);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(417, 136);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(75, 23);
+            this.btSend.TabIndex = 5;
+            this.btSend.Text = "&Send";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 181);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "GSM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btSend;
+    }
+}
+
---
+++ GSMMessage/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GSMMessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SerialPort sp = new SerialPort();
+                sp.PortName = txtPort.Text;
+                sp.Open();
+                sp.WriteLine("AT" + Environment.NewLine);
+
+                Thread.Sleep(100);
+                sp.WriteLine("AT+CMGF=1" + Environment.NewLine);
+
+                Thread.Sleep(100);
+                sp.WriteLine("AT+CSCS=\"GSM\"" + Environment.NewLine);
+
+                Thread.Sleep(100);
+                sp.WriteLine("AT+CMGS=\"" + txtNumber.Text + "\"" + Environment.NewLine);
+
+                Thread.Sleep(100);
+                sp.WriteLine(txtMessage.Text.Trim() + Environment.NewLine);
+
+                Thread.Sleep(100);
+                sp.Write(new byte[] { 26 }, 0, 1);
+
+                Thread.Sleep(100);
+                var response = sp.ReadExisting();
+                if (response.Contains("ERROR"))
+                    MessageBox.Show("Send failed!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("SMS Sent!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                sp.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
